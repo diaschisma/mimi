6f4f314907172c86e000ca7b59aef441630611ba
Сухарик <suhr@i2pmail.org>
2018-06-08 15:39:47 +0300

    Support different edos

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ extern crate miosc;
 
 extern crate clap;
 
-fn midi_pitch(midi: u8, ref_key: f32, ref_pitch: f32) -> f32 {
-    (midi as f32 - ref_key) * 12.0 / 31.0 + ref_pitch
+fn midi_pitch(midi: u8, edo: f32, ref_key: f32, ref_pitch: f32) -> f32 {
+    (midi as f32 - ref_key) * 12.0 / edo + ref_pitch
 }
 
 fn encode_miosc(msg: miosc::MioscMessage) -> Vec<u8> {
@@ -34,6 +34,13 @@ fn main() {
             .help("The reference pitch, in 12edo steps from MIDI 0")
             .takes_value(true)
         ).arg(
+            clap::Arg::with_name("edo")
+            .short("e")
+            .long("edo")
+            .value_name("edo")
+            .help("The number of divisions of the octave")
+            .takes_value(true)
+        ).arg(
             clap::Arg::with_name("address")
             .help("The OSC address to send messages to")
             .index(1)
@@ -45,8 +52,9 @@ fn main() {
     let address = matches.value_of("address").unwrap_or("localhost:3579");
     let socket = ::std::net::UdpSocket::bind("localhost:9753").unwrap();
 
-    let ref_key: f32 = matches.value_of("ref_key").and_then(|k| k.parse().ok()).unwrap_or(60.0);
-    let ref_pitch: f32 = matches.value_of("ref_pitch").and_then(|k| k.parse().ok()).unwrap_or(60.0);
+    let ref_key = matches.value_of("ref_key").and_then(|k| k.parse().ok()).unwrap_or(60.0);
+    let ref_pitch = matches.value_of("ref_pitch").and_then(|k| k.parse().ok()).unwrap_or(60.0);
+    let edo = matches.value_of("edo").and_then(|k| k.parse().ok()).unwrap_or(31.0);
 
     loop {
         if let Ok(Some(ev)) = midi_in.read() {
@@ -58,7 +66,7 @@ fn main() {
                 0x90 => {
                     let msg = MioscMessage::NoteOn(
                         msg.data1 as _,
-                        midi_pitch(msg.data1, ref_key, ref_pitch),
+                        midi_pitch(msg.data1, edo, ref_key, ref_pitch),
                         msg.data2 as f32 / 127.0
                     );
 
